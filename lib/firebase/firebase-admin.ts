b6b992ca9f905e25f2041d8ea6a4f1b171699c5c
Gibeom Choi <kcdevdes@gmail.com>
2025-02-05 23:33:29 -0500

    hotfix: add firebase-admin dependency

--- lib/firebase/firebase-admin.ts
+++ lib/firebase/firebase-admin.ts
@@ -8,9 +8,12 @@ if (!admin.apps.length) {
     credential: isEmulator
       ? admin.credential.applicationDefault()
       : admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+          clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(
+            /\\n/g,
+            '\n'
+          ),
         }),
   });
 }
